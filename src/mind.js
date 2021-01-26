0b02e1cdb3805cdf7bdb13f35a1a1d852bd2380b
aliyilmaz <aliyilmaz.work@gmail.com>
2021-01-27 02:18:28 +0300

    mind.js has been upgraded to version 1.0.1.

--- src/mind.js
+++ src/mind.js
@@ -1,3 +1,13 @@
+/**
+ *
+ * @package    mind.js
+ * @version    Release: 1.0.0
+ * @license    GPL3
+ * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
+ * @category   Javascript Framework, Basic web development kit.
+ * @link       https://github.com/aliyilmaz/mind.js
+ *
+ */
 function formSerialize(element){
     var elements = document.querySelector(element);
     var formData = new FormData(elements);
@@ -21,6 +31,24 @@ function actionPost(url, data, callback) {
 
     // Send request with data
     xhttp.send(data);
+    
+} 
+
+function actionGet(url, callback) {
+
+    var xhttp = new XMLHttpRequest();
+
+    // Set GET method and ajax file path
+    xhttp.open("GET", url, true);
+
+    // call on request changes state
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if(callback) callback(this.responseText);
+        }
+    };
+
+    xhttp.send(null);
     
 } 
 
